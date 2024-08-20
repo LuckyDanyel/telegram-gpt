d3c50e948b0dcb6379a4397351d07c231e33fe1b
Daniil <daniil.tereshchenkov@travelata.ru>
2024-08-20 22:11:41 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors({
-        origin: ['http://gpt.test-luckydanyel.ru'],
+        origin: ['https://test-luckydanyel.ru'],
     });
     app.use(cookieParser());
     await app.listen(3002);
