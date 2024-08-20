4c0e5b098b2312806ef2b2e3d60d1759183fe699
Daniil <daniil.tereshchenkov@travelata.ru>
2024-08-21 00:22:28 +0300

    fix

--- src/services/DialogService.ts
+++ src/services/DialogService.ts
@@ -37,7 +37,7 @@ export default class DialogService {
             }
 
             await this.cacheManager.set(newDialog.id, JSON.stringify(newDialog), this.cacheTime);
-            response.cookie('dialogId', newDialog.id, { httpOnly: true, secure: true });
+            response.cookie('dialogId', newDialog.id, { httpOnly: true, secure: true, domain: 'luckydanyel.ru' });
             return newDialog;
         } catch (error) {
             throw new BaseException({
